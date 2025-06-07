8bfde5726b93491ef490338af9b9bfccdeab2a66
Vaishnavi Patil <patilvaishnavi2012@gmail.com>
2025-06-07 18:05:52 +0530

    Initial commit

---
+++ app/src/main/java/com/firstpage/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.firstpage.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Linking views
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_btn);
+
+        // Set click listener
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                // Debug or show something
+                System.out.println("Username: " + username + ", Password: " + password);
+
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                intent.putExtra("USERNAME", username);
+                startActivity(intent);
+
+            }
+        });
+
+        // Handling window insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/firstpage/myapplication/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.firstpage.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    TextView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome_);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        welcomeText = findViewById(R.id.welcome_text);
+        String username = getIntent().getStringExtra("USERNAME");
+        if (username == null || username.isEmpty()) {
+            username = "Guest";
+        }
+        welcomeText.setText("Welcome, " + username + "!");
+
+    }
+}
